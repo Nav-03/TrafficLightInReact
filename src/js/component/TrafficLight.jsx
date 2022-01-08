6032f8110843dcc7344bd8b72d17cfa5c77c9539
Nav-03 <navil.gabrie@gmail.com>
2022-01-08 01:02:15 +0000

    initial commit

---
+++ src/js/component/TrafficLight.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TrafficLight = () => {
+	return (
+		<div className="bigDiv">
+			<div className="stem"></div>
+			<div className="lightHousing">
+				<div className="red-light"></div>
+				<div className="yellow-light"></div>
+				<div className="green-light"></div>
+			</div>
+		</div>
+	);
+};
+
+export default TrafficLight;

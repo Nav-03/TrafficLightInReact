b092a739706486f52d9993f3c5569a7f24a69333
Nav-03 <navil.gabrie@gmail.com>
2022-01-08 01:47:31 +0000

    final for submission

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TrafficLight = () => {
+	const [color, setColor] = useState("");
 	return (
 		<div className="bigDiv">
 			<div className="stem"></div>
 			<div className="lightHousing">
-				<div className="red-light"></div>
-				<div className="yellow-light"></div>
-				<div className="green-light"></div>
+				<div
+					onClick={() => {
+						setColor("red");
+					}}
+					className="red-light"
+					style={{
+						backgroundColor: color === "red" ? "purple" : "red",
+					}}></div>
+
+				<div
+					onClick={() => {
+						setColor("yellow");
+					}}
+					className="yellow-light"
+					style={{
+						backgroundColor:
+							color === "yellow" ? "purple" : "yellow",
+					}}></div>
+
+				<div
+					onClick={() => {
+						setColor("green");
+					}}
+					className="green-light"
+					style={{
+						backgroundColor: color === "green" ? "purple" : "green",
+					}}></div>
 			</div>
 		</div>
 	);
